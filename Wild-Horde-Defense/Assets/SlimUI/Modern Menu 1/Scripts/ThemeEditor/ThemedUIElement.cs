0762807ec1640357f2ea15592a2634188eab0e78
yugi-classic <93491287+MartinScheck@users.noreply.github.com>
2023-11-29 02:17:53 +0100

    adding MenuScene and HelpScene

---
+++ Wild-Horde-Defense/Assets/SlimUI/Modern Menu 1/Scripts/ThemeEditor/ThemedUIElement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SlimUI.ModernMenu{
+	[System.Serializable]
+	public class ThemedUIElement : ThemedUI {
+		[Header("Parameters")]
+		Color outline;
+		Image image;
+		GameObject message;
+		public enum OutlineStyle {solidThin, solidThick, dottedThin, dottedThick};
+		public bool hasImage = false;
+		public bool isText = false;
+
+		protected override void OnSkinUI(){
+			base.OnSkinUI();
+
+			if(hasImage){
+				image = GetComponent<Image>();
+			}
+
+			message = gameObject;
+
+			if(isText){
+				message.GetComponent<TextMeshPro>().color = themeController.textColor;
+			}
+		}
+	}
+}
