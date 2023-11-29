d8077399335fee6c0cc7fb5e4a77b939a7e226ba
yugi-classic <93491287+MartinScheck@users.noreply.github.com>
2023-11-29 13:26:45 +0100

    adding Pause Menu

---
+++ Wild-Horde-Defense/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;        //global var can be use to make sth
+    public GameObject pauseIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        pauseIcon.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        pauseIcon.SetActive(false);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        pauseIcon.SetActive(true);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void BacktoMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("GameMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
