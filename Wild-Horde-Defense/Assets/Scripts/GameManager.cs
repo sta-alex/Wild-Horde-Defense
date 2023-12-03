7655f4f124acd547cd2b0d21e8c51e15d4f14a35
yugi-classic <93491287+MartinScheck@users.noreply.github.com>
2023-12-03 19:37:35 +0100

    readjust money

--- Wild-Horde-Defense/Assets/Scripts/GameManager.cs
+++ Wild-Horde-Defense/Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currency = 5000;
+        currency = 220;
         currencyText.text = "$ " + currency;
         Debug.Log(currencyText);
     }
